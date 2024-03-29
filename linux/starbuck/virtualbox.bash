8640d914e6cb05e2f748e630a8884b7c466c19cd
Michael Soh <2767724+sohmc@users.noreply.github.com>
2024-03-29 17:27:29 -0400

    Download and install VirtualBox
    and set starbuck smb mounts to not be readonly

--- linux/starbuck/virtualbox.bash
+++ linux/starbuck/virtualbox.bash
@@ -10,10 +10,11 @@ echo "Installing VirtualBox"
 echo "  Installing VirtualBox gpg keys..."
 curl -fsSL https://www.virtualbox.org/download/oracle_vbox_2016.asc | gpg --yes --dearmor --output /usr/share/keyrings/oracle-virtualbox-2016.gpg
 
-echo "  Adding repository to /etc/apt/sources.list.d"
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/oracle-virtualbox-2016.gpg] https://download.virtualbox.org/virtualbox/debian ${DISTRO_NAME} contrib" | tee /etc/apt/sources.list.d/virtualbox.list > /dev/null
+echo "  Downloading VirtualBox"
+echo "  Please note that you are downloading a hard-coded version."
+curl -fsSL -o /tmp/virtualbox.deb "https://download.virtualbox.org/virtualbox/7.0.14/virtualbox-7.0_7.0.14-161095~Ubuntu~jammy_amd64.deb"
+
+apt install /tmp/virtualbox.deb
 
 apt-get update
-
-apt-get install -y virtualbox-7
 
